6d411bf1795126d9d292168383d2650b7d34211a
afrafaris72 <afra.faris123@gmail.com>
2018-05-17 20:09:07 +0700

    senwar

--- lib/sen_war.py
+++ lib/sen_war.py
@@ -3,3 +3,5 @@ import serial
 #from __future__ import print_function
 from datetime import date, datetime, timedelta
 import mysql.connector
+
+cnx = mysql.connector.connect(user='root', database='sensor')
