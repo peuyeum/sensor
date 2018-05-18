a1237e6d017e170076055d41392e8c65c47a009a
salwaatania <salwa.tania19@gmail.com>
2018-05-18 19:37:33 +0700

    ini

--- lib/UltramanSonic.py
+++ lib/UltramanSonic.py
@@ -5,3 +5,5 @@ def readm():
 	ArduinoSerial = serial.Serial('COM4', 9600)
 	conn=MySQLdb.connect(host='localhost', user='root', passwd='mnabil')
 	cursor = conn.cursor()
+	try:
+		curso.execute('use python_db')
