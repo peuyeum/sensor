ef567596a977a1a019b76e04b2a3a6ceaa18c3fc
sspitzenreiter <luthfimnabil17@gmail.com>
2018-05-22 00:20:01 +0700

    last fix

--- lib/UltramanSonic.py
+++ lib/UltramanSonic.py
@@ -1,39 +1,49 @@
 import serial
 import MySQLdb
+import psycopg2
 
-def readm(hos, use, pas, por, dbname):
-	ArduinoSerial = serial.Serial('COM4', 9600)
-	conn=MySQLdb.connect(host='localhost', user='root', passwd='mnabil')
-	cursor = conn.cursor()
+def myInput(hos, use, pas, por, dbname):
+	ArduinoSerial = serial.Serial(por, 9600)
+	conn=MySQLdb.connect(hos, use, pas, dbname)
+	cursor = conn.cursor()	
 	try:
-		curso.execute('use python_db')
-		except MySQLdb.OperationalError, message:
-		errorget = '(1094, "Unknown database \'python_db\'")'
+		result = cursor.execute("select * from py_ultraman_sonic")
+	except MySQLdb.ProgrammingError, message:
+		errorget = "(1146, \"Table '"+dbname+".bar' doesn't exist\")"
 		varis = str(message)==errorget
 		print errorget
 		print str(message)
-		if varis:
-			cursor.execute('create database python_db')
-		while True:
-			readed = str(ArduinoSerial.readline())
-			readlen = len(readed)-2
-			cursor.execute("INSERT INTO py_test(jarak) VALUES(concat(%s,'cm'))", [readed[0:readlen]])
-			conn.commit()
-			print ("INSERT INTO py_test(jarak) VALUES(concat(%s,'cm'))", [readed[0:readlen]])
+		if varis:			
+			cursor.execute('CREATE TABLE py_ultraman_sonic(id INT(3) NOT NULL PRIMARY KEY AUTO_INCREMENT, jarak VARCHAR(20) NOT NULL,waktu TIMESTAMP DEFAULT CURRENT_TIMESTAMP)')
+	while True:
+		readed = str(ArduinoSerial.readline())
+		readlen = len(readed)-2		
+		cursor.execute("INSERT INTO py_test(jarak) VALUES(concat(%s,'cm'))", [readed[0:readlen]])
+		conn.commit()
+		print ("INSERT INTO py_test(jarak) VALUES(concat(%s,'cm'))", [readed[0:readlen]])
 
 def posInput(hos, use, pas, por, dbname):
 	ArduinoSerial = serial.Serial(por, 9600)
-	conn=psycopg2.connect("dbname='"+dbname+"' user='"+use+"' password='"+pas+"'")cursor = conn.cursor()
+	conn=psycopg2.connect("dbname='"+dbname+"' user='"+use+"' password='"+pas+"'")
+	cursor = conn.cursor()
 	try:
-		result = cursor.execute("select * from py_ultraman_sonic")		
+		result = cursor.execute("select * from py_ultraman_sonic")
+	except psycopg2.ProgrammingError, message:
 		conn.rollback()
 		errorget = "relation \"py_ultraman_sonic\" does not exist\nLINE 1: select * from bar"
 		varis = str(message)[0:43]==errorget[0:43]
 		print errorget[0:43]
 		print str(message)[0:43]
-		print varis		
+		print varis
 		if varis:
 			cursor.execute('CREATE TABLE py_ultraman_sonic(id SERIAL, jarak VARCHAR(20) NOT NULL)')
 	while True:
 		readed = str(ArduinoSerial.readline())
 		readlen = len(readed)-2		
+		cursor.execute("INSERT INTO py_ultraman_sonic(jarak) VALUES(concat(%s,'cm'))", [readed[0:readlen]])
+		conn.commit()
+		print ("INSERT INTO py_ultraman_sonic(jarak) VALUES(concat(%s,'cm'))", [readed[0:readlen]])	
+
+		
+#myInput("localhost", "root", "mnabil", "COM4", "python_db_ultraman")
+#posInput("localhost", "postgres", "mnabil17", "COM4", "python_db")
