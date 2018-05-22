eafef401b1f18907d2e39aca207973ab8291711f
FaisalNajibAbdullah <32730169+FaisalNajibAbdullah@users.noreply.github.com>
2018-05-22 22:25:53 +0700

    commit6

--- lib/SensorGas.py
+++ lib/SensorGas.py
@@ -11,11 +11,22 @@ skrg = datetime.now()
 ser = serial.Serial('COM6', 9600)
 temp = ''
 while 1:
-data=ser.readline().rstrip('\n')
-#print data
-data=data.strip()
-print ('--'+ data)
+	data=ser.readline().rstrip('\n')
+	#print data
+	data=data.strip()
+	print ('--'+ data)
 
-add_logdata = ("INSERT INTO logdata  "
+	add_logdata = ("INSERT INTO logdata  "
 			"(kadar_gas,Tanggal) "
 			"VALUES (%s, %s)")
+	
+	dataB = (data, skrg)
+
+	# Insert new employee
+	cursor.execute(add_logdata, dataB)
+
+	# Make sure data is committed to the database
+	cnx.commit()
+	
+cursor.close()
+cnx.close()
